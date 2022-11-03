b4e7ae20956ab8b8bd46e4dfa31d126419d5e42a
daniel kanu <danielkanujr@gmail.com>
2022-11-03 05:33:32 -0500

    new updates

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3amplifyappstorage0b480369": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
